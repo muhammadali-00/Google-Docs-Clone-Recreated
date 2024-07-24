86e6a62a5cade30e0feca95b7e352a39f81b6885
Muhammad Ali <127246318+muhammadali-00@users.noreply.github.com>
2024-07-24 23:36:05 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import TextEditor from "./TextEditor";
+
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route,
+  Redirect
+ } from 'react-router-dom'
+
+
+
+function App() {
+  return <TextEditor/>
+  
+}
+
+
+export default App;
